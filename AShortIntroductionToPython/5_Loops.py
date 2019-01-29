153468e04bd87d5e14977c1d9a2d3a0752a4d966
Adam Peterlein <apeterlein@gmail.com>
2019-01-28 20:44:53 -0500

    Reordered and created new files.

--- AShortIntroductionToPython/5_Loops.py
+++ AShortIntroductionToPython/5_Loops.py
@@ -1,1 +1,6 @@
+# A Short Introduction To Python
+# FILE 5 - LOOPS
+# Adam Peterlein - Last updated 2019-01-28 with python 3.6.5 and Visual Studio 15.7.1.
+# Any questions, suggestions, or comments welcome and appreciated.
+
 
